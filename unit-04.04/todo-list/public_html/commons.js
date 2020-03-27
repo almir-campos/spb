1cbd3eedd1eedea4e638ea897175a66fe40948e9
Almir Campos <almircampos@gmail.com>
2020-03-26 17:55:17 -0700

    [WIP][unit-04.04][todo-list] Several implementations done. Others pending

--- unit-04.04/todo-list/public_html/commons.js
+++ unit-04.04/todo-list/public_html/commons.js
@@ -4,32 +4,146 @@ const defaults = [
 
 let todo;
 
-let saveTodo = function( json )
-{
-  console.log( 'Saving todo...');
-  localStorage.setItem( 'todo', JSON.stringify( json ) );
-};
+let saveTodo = function (json)
+  {
+    console.log('Saving todo...');
+    localStorage.setItem('todo', JSON.stringify(json));
+  };
 
-let saveDefaultTodo = function()
-{
-  console.log( 'Saving default todo...');
-  saveTodo( defaults );
-};
+let saveDefaultTodo = function ()
+  {
+    console.log('Saving default todo...');
+    saveTodo(defaults);
+  };
 
-let hasSavedData = function()
-{
-  return todo !== null && todo !== undefined;
-};
+let hasSavedData = function ()
+  {
+    return todo !== null && todo !== undefined;
+  };
 
-let toggleOptions = function( optionsDiv )
-{
+let toggleOptions = function (optionsDiv)
+  {
 
-};
+  };
+
+let addItem = function (elem)
+  {
+
+    let item = document.createElement('div');
+    item.classList.add('item', 'not-clicked');
+
+    let text = document.createElement('textarea');
+    console.log('text elem:', elem);
+    text.classList.add('text');
+    text.toggleAttribute('disabled');
+
+    let options = document.createElement('div');
+    options.classList.add('options');
+
+    let done = document.createElement('div');
+    done.classList.add('option', 'done');
+
+    let remove = document.createElement('div');
+    remove.classList.add('option', 'remove');
+
+    let doneOptionContent = document.createElement('span');
+    doneOptionContent.classList.add('option-content');
+    doneOptionContent.innerHTML = '&#10004;';
+
+    let removeOptionContent = document.createElement('span');
+    removeOptionContent.classList.add('option-content');
+    removeOptionContent.innerHTML = '&#x274C;';
+
+    //
+
+    done.appendChild(doneOptionContent);
+    remove.appendChild(removeOptionContent);
+    options.appendChild(done);
+    options.appendChild(remove);
+    item.append(text);
+    item.appendChild(options);
+
+    //
+
+    elem.appendChild(item);
+
+//    let htmlStr = 
+//             '<div class="item">'
+//            +'  <textarea class="text" rows="3" cols="4"></textarea>'
+//            +'  <div class="options">'
+//            +'    <div class="option done"><span class="option-content">&#10004;</span></div>'
+//            +'    <div class="option remove"><span class="option-content">&#x274C;</span></div>'
+//            +'  </div>'
+//            +'</div>';
+//    elem.innerHTML += htmlStr;
+  };
+
+let removeItem = function (elem)
+  {
+    elem.remove();
+  };
+
+let allItems = function ()
+  {
+    return document.querySelectorAll('.item');
+  };
+
+let allTextareas = function ()
+  {
+    return document.querySelectorAll('textarea');
+  };
+
+let bulkToggleClass = function (toRemove, toAdd)
+  {
+//    console.log(0, toRemove, toAdd);
+    let elems = document.querySelectorAll('.' + toRemove);
+//    console.log(1, elems);
+
+    if (elems.isEmpty())
+      {
+        return;
+      }
+//    console.log(elems);
+    elems.forEach((elem) =>
+    {
+      if (elem.classList.contains(toRemove))
+        {
+          console.log('removing:', toRemove);
+          elem.classList.replace(toRemove, toAdd);
+        }
+      else if (!elem.classList.contains(toAdd))
+        {
+          console.log('adding: ', toAdd);
+          elem.classList.add(toAdd);
+        }
+    });
+  };
+
+let disableTextareas = function ()
+  {
+    let textareas = allTextareas();
+    textareas.forEach(textarea =>
+    {
+//      if (!textarea.classList.isEmpty())
+//        {
+          console.log('removing...', textarea.classList );
+          textarea.classList.remove('is-editing');
+//        }
+      if (!textarea.hasAttribute('disabled'))
+        {
+          textarea.toggleAttribute('disabled');
+        }
+    });
+  };
 
 export {
-  defaults,
-  todo,
-  saveTodo,
-  saveDefaultTodo,
-  hasSavedData
-}
+defaults,
+    todo,
+    saveTodo,
+    saveDefaultTodo,
+    hasSavedData,
+    addItem,
+    removeItem,
+    bulkToggleClass,
+    disableTextareas
+    }
--- unit-04.04/todo-list/public_html/index.js
+++ unit-04.04/todo-list/public_html/index.js
@@ -1,17 +1,126 @@
 'use strict';
 
-import { saveDefaultTodo, hasSavedData } from './commons.js';
+import { saveDefaultTodo, hasSavedData, addItem, removeItem, bulkToggleClass, disableTextareas } from './commons.js';
+
+let mainDiv = document.querySelector('#main-div');
+let listDiv = document.querySelector('#list-div');
+
+addItem(listDiv);
 
 window.addEventListener('DOMContentLoaded', function ()
-{
-  if (!hasSavedData())
   {
-    saveDefaultTodo();
-  }
-});
+    if (!hasSavedData())
+      {
+        saveDefaultTodo();
+      }
+  });
 
-let listDiv = document.querySelector('#list-div');
-listDiv.addEventListener('mousemove', function (e)
-{
-      console.log('moving...','\n',  e.target.parentElement, '\n', e.currentTarget);
-});
+//let newDiv = document.querySelector('#new-div');
+//newDiv.addEventListener('click', function ()
+//  {
+//    disableTextareas();
+//     bulkToggleClass('clicked', 'not-clicked');
+//    addItem(listDiv);
+//  });
+
+mainDiv.addEventListener('click', function (e)
+  {
+//    e.preventDefault();
+     
+    let clicked = e.target;
+    console.log('cricked', clicked, 'clickedParent', clicked.parentElement);
+     bulkToggleClass('clicked', 'not-clicked');
+     
+     if ( clicked.id === 'new-div')
+     {
+       addItem(listDiv);
+     }
+    
+    
+     if ( ['main-div','top-div', 'new-div'].includes( clicked.id))
+     {
+       disableTextareas();
+       return;
+     }
+
+
+    if (clicked.classList.isEmpty())
+      {
+        disableTextareas();
+//     bulkToggleClass('clicked', 'not-clicked');
+
+        console.log('classList is empty');
+        return;
+      }
+
+    let isItem = clicked.classList.contains('item');
+    let isTextArea = clicked.classList.contains('text');
+
+    let clickedParent = clicked.parentElement;
+    let isDoneBt = clickedParent.classList.contains('done');
+    let isRemoveBt = clickedParent.classList.contains('remove');
+
+    let item = clicked.closest('.item');
+//    let isActive = item.classList.contains('clicked');
+//    let isEditing = item.querySelector('textarea').disabled;
+
+//    console.log('isEditing:', isEditing);
+
+//    bulkToggleClass('clicked', 'not-clicked');
+    
+    
+    console.log('Clicked on ', clickedParent, 'which parent of', clicked);
+
+//    let elem;
+//    if ( isItem || isTextArea )
+//    {
+//      elem = clicked;
+//    } else if ( isDoneBt || isRemoveBt )
+//    {
+//      
+//    }
+//    
+
+    if (isTextArea)
+      {
+        console.log('isTextArea');
+//        console.log( 'yyy', clicked, document.activeElement);
+        let isEditing = item.querySelector('textarea').disabled;
+        console.log("isEditing", isEditing );
+        disableTextareas();
+        if (isEditing)
+          {
+        clicked.toggleAttribute('disabled');
+        clicked.classList.add('is-editing');
+            clicked.focus();
+          }
+        else
+          {
+        clicked.classList.remove('is-editing');
+            console.log('is not Editing');
+          }
+      }
+    else if (isDoneBt)
+      {
+        disableTextareas();
+        console.log('isDoneBt');
+        console.log('Item Done!');
+      }
+    else if (isRemoveBt)
+      {
+        console.log('isRemoveBt');
+        removeItem(item);
+        console.log('Item has been removed.');
+      }
+    else if (isItem)
+      {
+        item.classList.replace('not-clicked', 'clicked');
+        console.log('isItemBt');
+        console.log('Edit item');
+      }
+    else
+      {
+        console.log('No action');
+      }
+  });
+
