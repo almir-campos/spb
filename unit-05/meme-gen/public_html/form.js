48586a90338d93602ae3799c64849ed61e5899fe
Almir Campos <almircampos@gmail.com>
2020-04-19 08:35:44 -0700

    [WIP][Unit 5][meme-gen] Form validation working

--- unit-05/meme-gen/public_html/form.js
+++ unit-05/meme-gen/public_html/form.js
@@ -1,5 +1,21 @@
 'use strict';
 
 export class Form {
-
+  static validate(e){
+    console.log( 'Form/validate');
+    const form = e.target;
+    let errors = {
+      top: '', url: '', btm: ''
+    };
+    if ( form.meme_top.value.toUpperCase().indexOf('BAD WORD') !== -1) {
+      errors.top = "Please, don't use bad words";
+    }
+    if ( form.meme_url.value.toUpperCase().indexOf('BAD WORD') !== -1) {
+      errors.url = "Please, don't use bad words";
+    }
+    if ( form.meme_btm.value.toUpperCase().indexOf('BAD WORD') !== -1) {
+      errors.btm = "Please, don't use bad words";
+    }
+    return errors;
+  }
 }
--- unit-05/meme-gen/public_html/main.js
+++ unit-05/meme-gen/public_html/main.js
@@ -1,2 +1,19 @@
 'use strict';
 
+console.clear();
+import { Form as formjs } from './form.js';
+
+const formDiv = document.querySelector('#form-div');
+// let topErr = document.querySelector('#meme-top-err').dataset.name;
+let topErr = document.querySelector('#meme-top-err');
+let urlErr = document.querySelector('#meme-url-err');
+let btmErr = document.querySelector('#meme-btm-err');
+formDiv.addEventListener('submit', (e) => {
+  console.log('dataset', topErr );
+  const valid = formjs.validate(e);
+  console.log('valid', valid);
+  topErr.innerText=valid.top;
+  urlErr.innerText=valid.url;
+  btmErr.innerText=valid.btm;
+  e.preventDefault();
+});
